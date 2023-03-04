bcfe67b4dc7859e80b13988625ac28f0d8a8a91b
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-03-04 19:51:10 -0300

    fix: rate limit times

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const { rateLimit } = require('express-rate-limit')
 
 const rateLimiter = rateLimit({
   windowMs: 1 * 60000, // 1 minute in milliseconds
-  max: 100,
+  max: 10,
   keyGenerator: (req) => req.ip,
   handler: (req, res) => res.status(429).json({ error: 'Limit of Requests achieved, wait a minute...' })
 })
